de9de2c0a775757b19439b1b8f259e8d2a0c6837
Yesenia <yesentorres@gmail.com>
2020-07-17 02:25:48 -0700

    began recommendation seed endpoint

--- main.py
+++ main.py
@@ -24,5 +24,23 @@ def get_access():
 def user_details():
   access_code = startup.TOKEN_DATA[0]
   auth_head = {"Authorization": "Bearer {}".format(access_code)} 
+  # get user info 
   user_info = requests.get('https://api.spotify.com/v1/me', headers=auth_head)
   return user_info.json()['display_name']
+
+# @app.route('/recommendation-generator', methods=['POST'])
+# @cross_origin()
+# def get_recommendation():
+#   # parse response
+#   result = request.json
+
+#   # set up preference params
+#   preferences = {
+    
+#   }
+#   # set up authorization header 
+#   access_code = startup.TOKEN_DATA[0]
+#   auth_head = {"Authorization": "Bearer {}".format(access_code)} 
+
+#   # get recommendations
+#   recommendation = requests.get('https://api.spotify.com/v1/recommendations?', headers=auth_head, params=preferences)
