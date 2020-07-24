2ac167c005fd62c37d62d7e8b105b3e1b02dc473
Yesenia <yesentorres@gmail.com>
2020-07-24 11:52:23 -0700

    cleanup

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from flask import Flask, jsonify, redirect, request, session, g
 from flask_cors import CORS, cross_origin
 import requests
 import startup
+import spotify_wrapper
 import json 
 
 app = Flask(__name__)
 CORS(app)
-
 
 @app.route('/')
 def index():
